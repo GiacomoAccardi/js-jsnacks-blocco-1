3588ea158e8fd6b34d72b769115bbd2cdbc78507
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-07-01 15:46:47 +0200

    [SNACK 1 UPD] There's now some instruction as comment inside the js file

---
+++ Snack1/script.js
@@ -0,0 +1,13 @@
+/*SNACK 1:
+In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+1 - Chiedere all'utente il nome tramite l'utilizzo di input.
+     ci sarà dunque la variabile "nome" che prenderà al, click di un pulsante, il valore inserito dall'utente.
+
+2 - Creare un array che contenga al suo interno tutti i nomi degli invitati 
+     Ci sarà dunque la variabile "invitati" che avrà come valore l'array
+
+3 - Avviare tramite ciclo un controllo che confronterà "invitati" con ogni dato contenuto nell'array e, qualora il dato non dovesse trovare un valore uguale a se stesso, verrà stampato un messaggio che nega l'accesso alla festa. OPPURE uno di benvenuto
+
+
+*/
