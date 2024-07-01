29463452afa0dcd96ab1233dac853850781528ff
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-07-01 16:56:34 +0200

    [snack 2] there are now some instructions into the js file

---
+++ Snack2/script.js
@@ -0,0 +1,11 @@
+/*
+SNACK 2:
+Crea un array vuoto.
+Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell'array
+
+1- Creare un array vuoto 
+2- Creare un ciclo for che per ogni ciclo:
+    >>Chiede tramite prompt un numero all'utente
+    >>Ne raccoglie il valore
+    >>lo pusha nell'array
+*/
