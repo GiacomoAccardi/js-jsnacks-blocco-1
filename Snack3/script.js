b171897eaff929c29c083e0b3d8d800958d17163
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-07-01 17:30:25 +0200

    [snack 3 FEAT] there's now some html elements in the page.

---
+++ Snack3/script.js
@@ -0,0 +1,9 @@
+/*Chiedi un numero di 4 cifre all’utente
+e calcola la somma di tutte le cifre che compongono il numero.
+
+1- chiedere un numero a quattro cifre all'utente
+2- scomporlo in quattro differenti valori
+3- effettuare la somma tra i valori 
+4- restituire un risultato.
+*/
+
